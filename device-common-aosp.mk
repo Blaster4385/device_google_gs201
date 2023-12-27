f7deefd18b9bdf97040694b5d5c74d6c9d0cb03a
Blaster4385 <TheTablaster@aospa.co>
2023-12-27 12:49:23 +0530

    gs201: Build Lineage Health HAL

--- device-common-aosp.mk
+++ device-common-aosp.mk
@@ -27,6 +27,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# Lineage Health
+include hardware/google/pixel/lineage_health/device.mk
+
 # NGA
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
